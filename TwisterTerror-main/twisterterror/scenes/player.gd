dfdbbd66d2f19f25333e3097205aa64cfc6e3c44
HBWright <166885620+HBWright@users.noreply.github.com>
2025-03-19 17:55:29 -0500

    Add files via upload

---
+++ TwisterTerror-main/twisterterror/scenes/player.gd
@@ -0,0 +1,26 @@
+class_name Player_Overworld extends CharacterBody2D
+
+@onready var anim = $AnimatedSprite2D
+@export var speed : float = 100
+@export var accel : float = 10
+var direction : Vector2
+
+func _ready():
+	anim.play()
+	
+func _physics_process(_delta: float) -> void:
+	direction.x = Input.get_axis("Left", "Right")
+	direction.y = Input.get_axis("Down", "Up")
+	direction = direction.normalized()
+	
+	if direction.x:
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, speed * direction.x, accel)
+	
+	if direction.y:
+		velocity.y = direction.y * speed
+	else:
+		velocity.y = move_toward(velocity.y, speed * direction.y, accel)
+		
+	move_and_slide()
