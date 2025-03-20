8cc7ca8e81c94c11bd88ed838bc86f2c92e3110f
HBWright <166885620+HBWright@users.noreply.github.com>
2025-03-20 13:27:26 -0500

    Worked on damaging tiles

---
+++ twisterterror/scenes/Objects/Level1/level_1.gd
@@ -0,0 +1,25 @@
+extends Area2D
+@onready var health = 10
+@onready var timer = $Timer
+
+func take_damage(num):
+	health -= num
+	if health == 0:
+		queue_free()
+		#add xp
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Area2D:
+		print("hello")
+		take_damage(5)
+		timer.start(1)
+	
+func _on_area_exited(area: Area2D) -> void:
+	if area is Area2D:
+		print("byebye")
+		timer.stop()
+		
+
+
+func _on_timer_timeout() -> void:
+	take_damage(5)
---
+++ twisterterror/scenes/manager.gd
@@ -0,0 +1,6 @@
+extends Control
+
+@onready var anim = $TV
+
+func _ready() -> void:
+	anim.play()
